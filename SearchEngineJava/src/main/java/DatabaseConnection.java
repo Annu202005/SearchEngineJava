c5a96607b5965c0226c9c966e504cb8c414fb453
Annu202005 <93881901+Annu202005@users.noreply.github.com>
2023-01-31 10:38:29 +0530

    Add files via upload

---
+++ SearchEngineJava/src/main/java/DatabaseConnection.java
@@ -0,0 +1,27 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    static Connection connection=null;
+
+    public static Connection getConnection(){
+        if(connection!=null){
+            return connection;
+        }
+        String user="root";
+        String password="123456780";
+        String url="jdbc:mysql://localhost:3306/searchenginejava2";
+        return getConnection(user,password,url);
+    }
+    private static Connection getConnection(String user,String password,String url){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection=DriverManager.getConnection(url,user,password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
